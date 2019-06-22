b83209726922b420723941305e6b0a2fdce91f71
Betsycampos <cbetsydalia@gmail.com>
2019-06-22 14:20:41 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import imag from './img/Captura.JPG';
 import './App.css';
-
+console.log(imag)
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="login-app">
+      <img src={imag} alt="Smiley face" height="42" width="42"/>
+
+      </div>
     </div>
   );
 }
