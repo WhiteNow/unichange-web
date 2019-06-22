0930c6c2859fee8971c9e4c024f11aa458f250af
Sofia Castillo <sofia19castle@gmail.com>
2019-06-22 17:49:09 -0500

    primera comna detalles convenio

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import {Navbar,Nav} from 'react-bootstrap'
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import Convenio from "./../Convenio";
 
 const NavigationNavBar = () => {
-    return <div>
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-        </Navbar>
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Navbar>
+      <div>
+        <Convenio />
+      </div>
     </div>
+  );
 };
 
 export default NavigationNavBar;
